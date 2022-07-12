d5749ea1f2f44ac57bdf03e0cdbf9e3493693e1f
Roy Selim <roybselim@outlook.com>
2022-07-12 15:38:23 +0800

    Added react navigation

--- App.tsx
+++ App.tsx
@@ -1,111 +1,23 @@
-import React, {type PropsWithChildren, useEffect, useState, useRef} from 'react';
-import { Dirs, FileSystem } from 'react-native-file-access';
+// In App.js in a new project
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  AppState,
-} from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/Home';
+import Details from './src/Details';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+const Stack = createNativeStackNavigator();
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [dirPresent, setDirectoryPresent] = useState<boolean>(false);
-  const [books, setBooks] = useState<string[]>([]);
-  const [error, setError] = useState<string>('');
-
-  const appState = useRef(AppState.currentState);
-  const [appStateVisible, setAppStateVisible] = useState(appState.current);
-
-  useEffect(() => {
-    const subscription = AppState.addEventListener("change", nextAppState => {
-      appState.current = nextAppState;
-      setAppStateVisible(appState.current);
-    });
-
-    return () => {
-      subscription.remove();
-    };
-  }, []);
-
-  const listBooks =  async (): Promise<void> => {
-    const books = await FileSystem.ls(Dirs.DocumentDir + '/books');
-    setBooks(books);
-  }
-
-  useEffect(() => {
-    const ifNoBooksDirCreateOne = async () => {
-      try {
-        await listBooks();
-      } catch (e) {
-        // Create a directory called 'books'
-        try{
-          await FileSystem.mkdir(Dirs.DocumentDir + '/books');
-          setDirectoryPresent(true);
-        } catch (e) {
-          setError('Error creating directoty')
-        }
-      }
-    }
-
-    ifNoBooksDirCreateOne();
-  }, [])
-
-  useEffect(() => {
-    if(dirPresent && appStateVisible) {
-      const listFilesInBooksDir = async () => {
-        try{
-          await listBooks();
-        } catch(e){
-          setError('Error checking for books');
-        }
-      }
-    
-      listFilesInBooksDir()
-    }
-  }, [dirPresent, appStateVisible])
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+function App() {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-          {books.map((book, index) => <View key={index}><Text>{book}</Text></View>)}
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Books">
+        <Stack.Screen name="Books" component={Home} />
+        <Stack.Screen name="Content" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+}
 
 export default App;
---
+++ src/Details.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { ScrollView, Text, StyleSheet } from 'react-native';
+import { Dirs, FileSystem } from 'react-native-file-access';
+
+interface IDetailsProps {
+	route: {
+		params: {
+			title: string;
+		}
+	}
+}
+
+const Details = (props: IDetailsProps) => {
+	const {params: {title}} = props.route;
+	const [content, setContent] = useState<string>('');
+	const [error, setError] = useState<string>('');
+
+	useEffect(() => {
+		const readBookContent = async () => {
+			try{
+				const bookContent = await FileSystem.readFile(`${Dirs.DocumentDir}/Books/${title}`);
+				setContent(bookContent);
+			} catch(e) {
+				setError('Error reading book content');
+			}
+		}
+
+		readBookContent();
+	}, [])
+
+  return (
+    <ScrollView style={styles.detailsContainer}>
+      <Text>
+				{content}
+			</Text>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    margin: 15,
+  }
+});
+
+export default Details;
